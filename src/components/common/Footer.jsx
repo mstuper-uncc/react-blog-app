7fc2d046f2440304e649853668c5ecdc75d35230
mstuper <mstuper@uncc.edu>
2025-11-12 11:06:45 -0500

    starting over

--- src/components/common/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer class="footer">
-      © {new Date().getFullYear()} My Blog • Copyright 2025
-    </footer>
-  );
-}
